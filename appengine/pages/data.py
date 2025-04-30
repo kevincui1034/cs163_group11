ee6168449b4124e70329a0149106f3b566a2dcee
Sang Tran <158090205+badcent@users.noreply.github.com>
2025-04-30 11:15:56 -0700

    Update data.py
    Added short explanation of data, will go more in depth later

--- appengine/pages/data.py
+++ appengine/pages/data.py
@@ -6,8 +6,13 @@ dash.register_page(__name__, path='/data')
 layout = html.Div([
     html.Div([
         html.H1('Data Page', style={'textAlign': 'center'}),
-        html.Div('Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed diam urna tempor. Pulvinar vivamus fringilla lacus nec metus bibendum egestas. Iaculis massa nisl malesuada lacinia integer nunc posuere. Ut hendrerit semper vel class aptent taciti sociosqu. Ad litora torquent per conubia nostra inceptos himenaeos.', 
-                style={'textAlign': 'center', 'maxWidth': '800px', 'margin': '0 auto'}),
+
+        html.Div([
+            html.P("We collected our data from a variety of sources. The major datasets we looked at were from Smogon and Kaggle."),
+            html.P("Within Smogon, we used competitive usage statistics, checks and counters, and sample sets from top-tier Pokémon."),
+            html.P("Kaggle provided structured Pokémon datasets including base stats, types, generations, and move pools."),
+            html.P("These sources allowed us to analyze how attributes like typing, base stats, and generation affect competitive performance."),
+        ], style={'textAlign': 'center', 'maxWidth': '800px', 'margin': '0 auto'})
     ], style={
         'display': 'flex',
         'flexDirection': 'column',
